86d68105c90cbf3451ae1a9b01301c1055a1bfa7
karlozrilic <kazrile@gmail.com>
2021-03-16 15:05:04 +0100

    import fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import MessegerBot from "./components/MessengerBot";
 import Home from "./layouts/Home";
 
 function App() {
