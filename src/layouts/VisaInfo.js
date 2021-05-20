be3bb050fc10b543de8cea2273f9596128f3cb10
karlozrilic <kazrile@gmail.com>
2021-05-20 18:19:15 +0200

    small fix

--- src/layouts/VisaInfo.js
+++ src/layouts/VisaInfo.js
@@ -1,7 +1,7 @@
 function VisaInfo() {
     return (
         <>
-            <div className="hero-image visa-info" id="visa">
+            <div className="hero-image visa-info">
                 <div className="hero-text">
                     <h1>Visa informations</h1>
                     <p>Here you can find all necessary information on how to get visa</p>
